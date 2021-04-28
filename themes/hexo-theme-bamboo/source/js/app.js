5a36ebf2d5a31f368c44f4a5ac9c38d14db8c724
wanghao <1286657882@qq.com>
2021-04-28 15:45:02 +0800

    安装测试

--- themes/hexo-theme-bamboo/source/js/app.js
+++ themes/hexo-theme-bamboo/source/js/app.js
@@ -29,6 +29,7 @@ const addBtn = document.querySelector('.add-button');
 addBtn.style.display = 'none';
 
 window.addEventListener('beforeinstallprompt', (e) => {
+    console.log('beforeinstallprompt');
   // Prevent Chrome 67 and earlier from automatically showing the prompt
   e.preventDefault();
   // Stash the event so it can be triggered later.
